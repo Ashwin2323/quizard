74d7d441b7650763cf146f412c351a67e4e31513
Ashwin2323 <ashwinhammar5555@gmail.com>
2025-09-30 23:58:25 +0530

    Route check for signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <div className=""> 
+    <div className="">
+      <BrowserRouter>
       <Navbar/>
-      <HomePage/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/signup" element={<Login/>}/>
+          <Route path="/Login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button"
+import {Link, useNavigate} from "react-router-dom"
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between h-[64px] bg-black text-white w-full px-[16px] py-[8px]">
       <h1 className="text-3xl font-bold bg-gradient-to-r from-purple-500 to-amber-500 bg-clip-text text-transparent">
@@ -12,7 +16,8 @@ const Navbar = () => {
         <h1 className="text-2xl"> About </h1>
       </div>
       <div className="flex gap-3">
-        <Button className="h-full bg-slate-200 text-black" >Sign Up</Button>
+        <Button className="h-full bg-slate-200 text-black" 
+        onClick={()=>{navigate("/signup")}} >Sign Up</Button>
         <Button className="h-full bg-violet-800" >Register</Button>
       </div>
     </div>
